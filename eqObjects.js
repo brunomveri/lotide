26d314e821801585caecc3172ca4a0c80e40fa2e
Bruno Verissimo <70541301+brunomveri@users.noreply.github.com>
2021-01-06 20:46:48 -0700

    Update eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -3,7 +3,6 @@ const eqArrays = require("./eqArrays");
 const eqObjects = function(object1, object2) {
   const keysObj1 = Object.keys(object1);
   const keysObj2 = Object.keys(object2);
-
   if (keysObj1.length !== keysObj2.length) {
     return false;
   }
@@ -20,7 +19,6 @@ const eqObjects = function(object1, object2) {
       else if (object1[key] !== object2[key]) {
         return false;
       }
-      
     }
     return true;
   };
