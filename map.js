7e0f930b4a5482f6984b07ed072c954e620c1c53
vagrant <mveri.bruno@gmail.com>
2021-01-07 22:51:06 +0000

    Implement Map

---
+++ map.js
@@ -0,0 +1,36 @@
+const eqArrays = function(array1,array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for(let i = 0; i < array1.length; i++) {
+     if (array1[i] !== array2[i]) {
+       return false;
+     }
+   }
+   return true;
+};
+
+const assertArraysEqual = function(array1,array2) {
+  const returnBoolean = eqArrays(array1,array2);
+  if (returnBoolean) {
+    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+/************************************************************************/
+const map = function(array,callback) {
+  const results = [];
+  for (let item of array) {
+   results.push(callback(item));
+  }
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+
+console.log(results1);
+
+assertArraysEqual(results1, ["g","c","t","m","t"]);
