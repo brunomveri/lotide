1933f1a51f51e38537c263f729f0b3b0a2314a18
vagrant <mveri.bruno@gmail.com>
2021-01-11 22:22:40 +0000

    upgraded the test using mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,16 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+// const assertEqual = require('../assertEqual');
 const head = require('../head');
 
-//TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([true]), true);
-assertEqual(head([]), undefined);
-assertEqual(head(["b"]), "b");
+describe("#head", () => {
+
+  it("returns 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+
+});
