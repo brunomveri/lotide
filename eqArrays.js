a8b401a58a0d0fb1edd8b12bf39b1fd24df70696
vagrant <mveri.bruno@gmail.com>
2021-01-11 17:22:26 +0000

    cleaned eqArrys moving its test codes to a test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-//FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1,array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -19,9 +10,7 @@ const eqArrays = function(array1,array2) {
    return true;
 };
 
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
+
+
